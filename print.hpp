7c4f1ff19a3d740161ec2734e53d2b04f3ae33ee
belfastkeyboard <149603786+belfastkeyboard@users.noreply.github.com>
2023-11-06 19:59:02 +0000

    print.hpp refactor
    1. Class refactor, replace string concatenation with string streams. Simplifies the class and number of methods required.
    2. Removed <type_traits> header.
    3. Improved smart pointer support. No longer need .get() method in the function's arguments. .get() outputs the memory address now.
    4. Added support for the following container types: multiset, unordered_multiset, multimap, unordered_multimap, stack, queue, priority_queue.

--- print.hpp
+++ print.hpp
@@ -1,8 +1,4 @@
-#pragma once
-
-/*
- *
- *      Pythonic print() function
+/*      Pythonic print() function
  *      
  *      Simple and concise method of printing to console.
  *      print() is a variadic function, it accepts as many arguments as you want.
@@ -11,16 +7,14 @@
  *      print() the contents of your containers without creating your own loops or other specific access methods.
  *      Mix and match data types.
  * 
- *      Requires C++14.
- * 
  * 
  *      Currently supports all fundamental data-types:
  * 
  *          > bool (prints 'true' | 'false', not '1' | '0')
  *          > char
  *          > C style strings
- *          > C++ style strings e.g. std::string
- *          > all numeric values, int- or float-based, signed, unsigned, size_t, etc.
+ *          > C++ style strings
+ *          > all numeric types
  * 
  * 
  *      Supports these data-structures/containers:
@@ -32,22 +26,27 @@
  *          > std::list
  *          > std::deque
  *          > std::set
+ *          > std::multiset
  *          > std::unordered_set
+ *          > std::unordered_multiset
  *          > std::map
+ *          > std::multimap
  *          > std::unordered_map
+ *          > std::unordered_multimap
+ * 
+ *          Supported but only default container types have been tested:
+ *              > std::stack
+ *              > std::queue
+ *              > std::priority_queue
  *           
  * 
- *      Partial support for smart pointers:
+ *      Supports smart pointers:
  *          
- *          > std::unique_ptr (use ptr.get() in the print() function's argument)
+ *          > std::unique_ptr
  *          > std::shared_ptr
  *          > std::weak_ptr
  * 
- *          Smart pointer support limited to:
- * 
- *              > numeric types
- *              > bool
- *              Further implementation pending.
+ *              Currently no safety checks regarding undefined pointers. 
  * 
  * 
  *      Supports references (not fully tested.)
@@ -55,27 +54,32 @@
  * 
  *      Partial support for pointers:
  * 
- *          Pointer support limited to:
+ *          Pointer support limited to fundamental types.
+ *          Container support to be implemented.
+ *
  * 
- *          > bool
- *          > numeric types
+ *      ***          <---KNOWN ISSUES--->          ***
  *      
+ *      wchar_t representation
+ * 
  *     
  *      ***          <---TO DO--->          ***
  * 
- *      Some C++17 features:
+ *      C++17 features:
  * 
  *          std::variant<>
  *          std::optional<>
  *
  * 
  *      Further testing for reference& support.
- *      Add additional pointer* &support.
+ *      Add additional pointer* &support for containers.
  *      Support for print(&memory_address).
  * 
- *      Support for C-style arrays.
- *      Support for multidimensional vectors, arrays, etc.
- *      Support for unions.
+ *      Support for:
+ *          > std::bitset
+ *          > C-style arrays.
+ *          > multidimensional containers
+ *          > unions.
  *      
  * 
  *      Parameter support:
@@ -86,273 +90,346 @@
  *          > sep=""
  *          > end=""
  * 
- *      Refactor code:
- * 
- *          Implementation is currently a bit banjaxed and needs to be cleaned up.
- *
  *      Improve documentation
  *      
  *      Improve preprocessor directives
  * 
  *      Improve error handling
  * 
- * 
  */
 
-#if __cplusplus >= 201402L
+#ifndef     PYPRINT
+#define     PYPRINT 1
 
 #include    <iostream>
 #include    <string>
-#include    <typeinfo>
+#include    <sstream>
 
 class PyPrint
 {
 private:
-    typedef std::string    string;
-    string  output = "";
+    std::stringstream   stream;
+    std::string         output;
 
-    void print(bool sep, bool __arg)
-    {
-        output += ((__arg) ? "true" : "false");
-        if (sep) output += ' ';
-    }
-    void print(bool sep, bool* __arg)
-    {
-        output += ((*__arg) ? "true" : "false");
-        if (sep) output += ' ';
-    }
-    void print(bool sep, char __arg)
-    {
-        output += __arg;
-        if (sep) output += ' ';
-    }
-    void print(bool sep, wchar_t __arg)
-    {
-        output += __arg;
-        if (sep) output += ' ';
-    }
-    void print(bool sep, const char* __arg)
-    {
-        output.append(__arg);
-        if (sep) output += ' ';
-    }
-    void print(bool sep, string __arg)
-    {
-        output += __arg;
-        if (sep) output += ' ';
-    }
+    // fundamental data types 
     template <typename T>
-    void print(bool sep, T __arg)
+    void print(bool sep, T& __arg)
     {
-        if (std::is_integral_v<T>)
-        {
-            output += std::to_string(__arg);
-        }
-        else if (std::is_floating_point_v<T>)
-        {
-            std::string fl = std::to_string(__arg);
-            while (fl[fl.size() - 1] == fl[fl.size() - 2]) {fl.pop_back();}
-            output += fl;
-        }
-        if (sep) output += ' ';
+        stream << __arg;
+        if (sep) stream << " ";
     }
+    // fundamental type pointers
     template <typename T>
     void print(bool sep, T* __arg)
     {
-        if (std::is_integral_v<T>)
-        {
-            output += std::to_string(*__arg);
-        }
-        else if (std::is_floating_point_v<T>)
-        {
-            std::string fl = std::to_string(*__arg);
-            while (fl[fl.size() - 1] == fl[fl.size() - 2]) {fl.pop_back();}
-            output += fl;
-        }
-        if (sep) output += ' ';
+        stream << *__arg;
+        if (sep) stream << " ";
     }
-
-    #ifdef      _GLIBCXX_UTILITY        // std::pair
-    template <typename T, typename U>
-    void print(bool sep, std::pair<T, U> __arg)
+    // C style string handlers
+    void print(bool sep, char __arg[])
     {
-        output += '(';
-        print(false, __arg.first);
-        output += ", ";
-        print(false, __arg.second);
-        output += ')';
-        if (sep) output += ' ';
+        stream << __arg;
+        if (sep) stream << " ";
     }
+    void print(bool sep, const char* __arg)
+    {
+        stream << __arg;
+        if (sep) stream << " ";
+    }
+    // std::pair<T, U>
+    #ifdef      _GLIBCXX_UTILITY
+        template <typename T, typename U>
+        void print(bool sep, std::pair<T, U> __arg)
+        {
+            stream << "(";
+            print(false, __arg.first);
+            stream << ", ";
+            print(false, __arg.second);
+            stream << ")";
+            if (sep) stream << " ";
+        }
     #endif
-
-    #ifdef      _GLIBCXX_TUPLE          // std::tuple
+    // std::tuple<T...>
+    #ifdef      _GLIBCXX_TUPLE
     template <typename... T>
     void print(bool sep, std::tuple<T...>& __arg)
     {
-        output += '[';
+        stream << "[";
         print_tuple(false, __arg);
-        while (output.back() == ' ' || output.back() == ',') output.pop_back();
-        output += ']';
-        if (sep) output += ' ';
+        stream << "]";
+        if (sep) stream << " ";
     }
     template <size_t I = 0, typename... T>
     void print_tuple(bool sep, std::tuple<T...>& __arg)
     {
         if constexpr (I < sizeof...(T)) {
         print(false, std::get<I>(__arg));
-        output += ", ";
+        if (I + 1 < sizeof...(T)) stream << ", ";
         print_tuple<I + 1>(false, __arg);
         }
     }
     #endif
-
-    #ifdef      _GLIBCXX_ARRAY          // std::array
+    // std::array<T, U>
+    #ifdef      _GLIBCXX_ARRAY
     template <typename T, size_t U>
     void print(bool sep, std::array<T, U> __arg)
     {
-        output += '[';
+        stream << "[";
         for (auto it = __arg.begin(); it != __arg.end(); it++)
         {
             print(false, *it);
-            if (std::next(it) != __arg.end())  output += ", ";
+            if (std::next(it) != __arg.end()) stream << ", ";
         }
-        output += ']';
-        if (sep) output += ' ';
+        stream << "]";
+        if (sep) stream << " ";
     }
     #endif
-
-    #ifdef      _GLIBCXX_VECTOR         // std::vector
+    // std::vector<T>
+    #ifdef      _GLIBCXX_VECTOR
     template <typename T>
     void print(bool sep, std::vector<T> __arg)
     {
-        output += '[';
+        stream << "[";
         for (auto it = __arg.begin(); it != __arg.end(); it++)
         {
             print(false, *it);
-            if (std::next(it) != __arg.end())  output += ", ";
+            if (std::next(it) != __arg.end()) stream << ", ";
         }
-        output += ']';
-        if (sep) output += ' ';
+        stream << "]";
+        if (sep) stream << " ";
     }
     #endif
-
-    #ifdef      _STL_LIST_H             // std::list
+    // std::list<T>
+    #ifdef      _STL_LIST_H
     template <typename T>
     void print(bool sep, std::list<T> __arg)
     {
-        output += '[';
+        stream << "[";
         for (auto it = __arg.begin(); it != __arg.end(); it++)
         {
             print(false, *it);
-            if (std::next(it) != __arg.end())  output += ", ";
+            if (std::next(it) != __arg.end()) stream << ", ";
         }
-        output += "] ";
+        stream << "]";
+        if (sep) stream << " ";
     }
     #endif
-
-    #ifdef      _GLIBCXX_DEQUE          // std::deque
+    // std::deque<T>
+    #ifdef      _GLIBCXX_DEQUE
     template <typename T>
     void print(bool sep, std::deque<T> __arg)
     {
-        output += '[';
+        stream << "[";
         for (auto it = __arg.begin(); it != __arg.end(); it++)
         {
             print(false, *it);
-            if (std::next(it) != __arg.end())  output += ", ";
+            if (std::next(it) != __arg.end()) stream << ", ";
         }
-        output += ']';
-        if (sep) output += ' ';
+        stream << "]";
+        if (sep) stream << " ";
     }
     #endif
-
-    #ifdef      _GLIBCXX_SET            // std::set
+    // std::set<T>
+    #ifdef      _GLIBCXX_SET
     template <typename T>
     void print(bool sep, std::set<T> __arg)
     {
-        output += '[';
+        stream << "[";
         for (auto it = __arg.begin(); it != __arg.end(); it++)
         {
             print(false, *it);
-            if (std::next(it) != __arg.end())  output += ", ";
+            if (std::next(it) != __arg.end()) stream << ", ";
         }
-        output += ']';
-        if (sep) output += ' ';
+        stream << "]";
+        if (sep) stream << " ";
     }
     #endif
-
-    #ifdef      _GLIBCXX_UNORDERED_SET  // std::unordered_set
+    // std::multiset<T>
+    #ifdef      _STL_MULTISET_H
+    template <typename T>
+    void print(bool sep, std::multiset<T> __arg)
+    {
+        stream << "[";
+        for (auto it = __arg.begin(); it != __arg.end(); it++)
+        {
+            print(false, *it);
+            if (std::next(it) != __arg.end()) stream << ", ";
+        }
+        stream << "]";
+        if (sep) stream << " ";
+    }
+    #endif
+    // std::unordered_set<T>
+    #ifdef      _GLIBCXX_UNORDERED_SET
     template <typename T>
     void print(bool sep, std::unordered_set<T> __arg)
     {
-        output += '[';
+        stream << "[";
         for (auto it = __arg.begin(); it != __arg.end(); it++)
         {
             print(false, *it);
-            if (std::next(it) != __arg.end())  output += ", ";
+            if (std::next(it) != __arg.end()) stream << ", ";
         }
-        output += ']';
-        if (sep) output += ' ';
+        stream << "]";
+        if (sep) stream << " ";
     }
     #endif
-
-    #ifdef      _GLIBCXX_MAP            // std::map
+    // std::unordered_multiset<T>
+    #ifdef      _UNORDERED_SET_H
+    template <typename T>
+    void print(bool sep, std::unordered_multiset<T> __arg)
+    {
+        stream << "[";
+        for (auto it = __arg.begin(); it != __arg.end(); it++)
+        {
+            print(false, *it);
+            if (std::next(it) != __arg.end()) stream << ", ";
+        }
+        stream << "]";
+        if (sep) stream << " ";
+    }
+    #endif
+    // std::map<T, U>
+    #ifdef      _GLIBCXX_MAP
     template <typename T, typename U>
     void print(bool sep, std::map<T, U> __arg)
     {
-        output += '[';
+        stream << "[";
         for (auto it = __arg.begin(); it != __arg.end(); it++)
         {
             print(false, it->first);
-            output += ": ";
+            stream << ": ";
             print(false, it->second);
-            if (std::next(it) != __arg.end())  output += ", ";
+            if (std::next(it) != __arg.end()) stream << ", ";
         }
-        output += ']';
-        if (sep) output += ' ';
+        stream << "]";
+        if (sep) stream << " ";
     }
     #endif
-
-    #ifdef      _GLIBCXX_UNORDERED_MAP  // std::unordered_map
+    // std::multimap<T, U>
+    #ifdef      _STL_MULTIMAP_H
+    template <typename T, typename U>
+    void print(bool sep, std::multimap<T, U> __arg)
+    {
+        stream << "[";
+        for (auto it = __arg.begin(); it != __arg.end(); it++)
+        {
+            print(false, it->first);
+            stream << ": ";
+            print(false, it->second);
+            if (std::next(it) != __arg.end()) stream << ", ";
+        }
+        stream << "]";
+        if (sep) stream << " ";
+    }
+    #endif
+    // std::unordered_map<T, U>
+    #ifdef      _GLIBCXX_UNORDERED_MAP
     template <typename T, typename U>
     void print(bool sep, std::unordered_map<T, U> __arg)
     {
-        output += '[';
+        stream << "[";
         for (auto it = __arg.begin(); it != __arg.end(); it++)
         {
             print(false, it->first);
-            print(false, ": ");
+            stream << ": ";
             print(false, it->second);
-            if (std::next(it) != __arg.end())  output += ", ";
+            if (std::next(it) != __arg.end()) stream << ", ";
         }
-        output += ']';
-        if (sep) output += ' ';
+        stream << "]";
+        if (sep) stream << " ";
     }
     #endif
-
-    #ifdef      _GLIBCXX_MEMORY         // smart pointers
+    // std::unordered_multimap<T, U>
+    #ifdef      _UNORDERED_MAP_H
+    template <typename T, typename U>
+    void print(bool sep, std::unordered_multimap<T, U> __arg)
+    {
+        stream << "[";
+        for (auto it = __arg.begin(); it != __arg.end(); it++)
+        {
+            print(false, it->first);
+            stream << ": ";
+            print(false, it->second);
+            if (std::next(it) != __arg.end()) stream << ", ";
+        }
+        stream << "]";
+        if (sep) stream << " ";
+    }
+    #endif
+    // std::stack<T>
+    #ifdef      _GLIBCXX_STACK
+    template <typename T>
+    void print(bool sep, std::stack<T> __arg)
+    {
+        stream << "[";
+        while (!__arg.empty())
+        {
+            print(false, __arg.top());
+            __arg.pop();
+            if (__arg.size() > 0) stream << ", ";
+        }
+        stream << "]";
+        if (sep) stream << " ";
+    }
+    #endif
+    // std::queue<T>, std::priority_queue<T>
+    #ifdef      _GLIBCXX_QUEUE
+    template <typename T>
+    void print(bool sep, std::queue<T> __arg)
+    {
+        stream << "[";
+        while (!__arg.empty())
+        {
+            print(false, __arg.front());
+            __arg.pop();
+            if (__arg.size() > 0) stream << ", ";
+        }
+        stream << "]";
+        if (sep) stream << " ";
+    }
+    template <typename T>
+    void print(bool sep, std::priority_queue<T> __arg)
+    {
+        stream << "[";
+        while (!__arg.empty())
+        {
+            print(false, __arg.top());
+            __arg.pop();
+            if (__arg.size() > 0) stream << ", ";
+        }
+        stream << "]";
+        if (sep) stream << " ";
+    }
+    #endif
+    // std::unique_ptr<T>, std::shared_ptr<T>, std::weak_ptr<T>
+    #ifdef      _GLIBCXX_MEMORY
     template <typename T>
     void print(bool sep, std::unique_ptr<T>& __arg)
     {
-        print(true, __arg.get());
+        print(true, *__arg.get());
     }
     template <typename T>
     void print(bool sep, std::shared_ptr<T>& __arg)
     {
-        print(true, __arg.get());
+        print(true, *__arg.get());
     }
     template <typename T>
     void print(bool sep, std::weak_ptr<T>& __arg)
     {
-        print(true, __arg.get());
+        print(true, *__arg.lock());
     }
     #endif
 
 public:
     template <typename... Arg>
-    PyPrint(Arg... __args) {
+    PyPrint(Arg&&... __args)
+    {
+        stream << std::boolalpha;
         (print(true, __args), ...);
-        while (output.back() == ' ') {output.pop_back();}
-        output += '.';
+        std::string output = stream.str();
+        while (output.back() == ' ') output.pop_back();
+        if (output.back() != '!' && output.back() != '.') output += '.';
         std::cout << output << std::endl;
     }
 };
@@ -362,7 +439,7 @@ inline void print()
     std::cout << std::endl;
 }
 template <typename... T>
-void print(T... __args)
+void print(T&&... __args)
 {
     PyPrint python_print(__args...);
 }
